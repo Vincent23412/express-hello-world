18d58f13a31ddf76db8fd5fcd17a91ff9558aca3
Vincent23412 <87458133+Vincent23412@users.noreply.github.com>
2024-01-19 20:38:38 +0800

    app.js update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.get("/", (req, res) => res.type('html').send(html));`: 
+app.get("/", (req, res) => res.type('html').send(html));
 
 // Add a new route for the health endpoint 
 
