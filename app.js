8ae430b30f304b448d592624ac86eb6782623b16
陳敬翰 <87458133+Vincent23412@users.noreply.github.com>
2024-01-19 20:32:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/health", (req, res) => {
 
 }); 
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(port));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
